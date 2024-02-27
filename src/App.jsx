9664a7b038ac7f3eade6b65a10c332ce8eb7a274
Mezekr W <mezi.edu@tuta.io>
2024-02-27 20:15:56 +0100

    add Mainview to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css';
+import MainView from './components/MainView/MainView';
 import './index.scss';
 
 function App() {
 	return (
-		<div className="my-flix">
-			<div>Welcome to MeziFlix Movies App 🥳🤗</div>
-		</div>
+		<>
+			<div className="my-flix">
+				<div>Welcome to MeziFlix Movies App 🥳🤗</div>
+			</div>
+			<MainView />
+		</>
 	);
 }
 
