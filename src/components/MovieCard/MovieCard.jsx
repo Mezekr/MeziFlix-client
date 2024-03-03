754764757771f58a054cd5c3c1e34d0e4da2b96b
Mezekr W <mezi.edu@tuta.io>
2024-03-03 09:22:48 +0100

    refactor movies list to MovieCard type

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -3,16 +3,21 @@ import PropTypes from 'prop-types';
 const MovieCard = ({ movie, OnMovieClick }) => {
 	return (
 		<div
+			className="movies-list"
 			onClick={() => {
 				OnMovieClick(movie);
 			}}
 		>
+			<img src={movie.ImagePath} alt={movie.Title} />
 			{movie.Title}
 		</div>
 	);
 };
 MovieCard.propTypes = {
-	movie: PropTypes.object,
+	movie: PropTypes.shape({
+		ImagePath: String,
+		Title: String,
+	}),
 	OnMovieClick: PropTypes.func,
 };
 
