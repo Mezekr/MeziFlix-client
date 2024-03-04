ce28bb50a915006000c25ab2fc4bb6aa0b2a6ea5
Mezekr W <mezi.edu@tuta.io>
2024-03-04 20:42:49 +0100

    integrate login & signup to main view

--- src/components/MainView/MainView.jsx
+++ src/components/MainView/MainView.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
+import LoginView from '../LoginView/LoginView';
 import MovieCard from '../MovieCard/MovieCard';
 import MovieView from '../MovieView/MovieView';
+import SignupView from '../SignupView/SignupView';
 
 // const MOVIES_API_URL = import.meta.env.REACT_APP_API_URL;
 // console.log(MOVIES_API_URL);
@@ -9,6 +11,8 @@ const MOVIES_API_URL = 'https://meziflix-api-v1.onrender.com/';
 
 const MainView = () => {
 	const [movies, setMovies] = useState([]);
+	const [selectedMovie, setSelectedMovie] = useState(null);
+	const [user, setUser] = useState('null');
 
 	const searchMovie = async () => {
 		const response = await fetch(`${MOVIES_API_URL}movies`);
@@ -20,7 +24,7 @@ const MainView = () => {
 		searchMovie();
 	}, []);
 
-	const [selectedMovie, setSelectedMovie] = useState(null);
+	if (!user) return <LoginView onLogin={(user) => setUser(user)} />;
 
 	if (selectedMovie) {
 		const setSimilarMovies = movies.filter(
@@ -54,27 +58,30 @@ const MainView = () => {
 	}
 
 	return (
-		<div>
-			{movies?.length <= 0 ? (
-				<div className="empty">
-					<h2> loading...</h2>
-				</div>
-			) : (
-				<div className="movie-container">
-					{movies.map((movie) => {
-						return (
-							<MovieCard
-								key={movie._id}
-								movie={movie}
-								OnMovieClick={(newSelectedMovie) =>
-									setSelectedMovie(newSelectedMovie)
-								}
-							/>
-						);
-					})}
-				</div>
-			)}
-		</div>
+		<>
+			<SignupView />
+			<div>
+				{movies?.length <= 0 ? (
+					<div className="empty">
+						<h2> loading...</h2>
+					</div>
+				) : (
+					<div className="movie-container">
+						{movies.map((movie) => {
+							return (
+								<MovieCard
+									key={movie._id}
+									movie={movie}
+									OnMovieClick={(newSelectedMovie) =>
+										setSelectedMovie(newSelectedMovie)
+									}
+								/>
+							);
+						})}
+					</div>
+				)}
+			</div>
+		</>
 	);
 };
 export default MainView;
