f224b67dbb2d75f4d33a862beaa06082e947f574
Mezekr W <mezi.edu@tuta.io>
2024-03-09 10:39:38 +0100

    refactor MovieCard with Bootstrap's Card component

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,22 +1,32 @@
 import PropTypes from 'prop-types';
+import { Button, Card } from 'react-bootstrap';
 
 const MovieCard = ({ movie, OnMovieClick }) => {
 	return (
-		<div
-			className="movies-list"
-			onClick={() => {
-				OnMovieClick(movie);
-			}}
-		>
-			<img src={movie.ImagePath} alt={movie.Title} />
-			{movie.Title}
-		</div>
+		<Card style={{ width: '18rem' }} className="h-100">
+			<Card.Img src={movie.ImagePath} alt={movie.Title} />
+			<Card.Body>
+				<Card.Title>Title: {movie.Title}</Card.Title>
+				<Card.Text>Director: {movie.Director.Name}</Card.Text>
+			</Card.Body>
+			<Button
+				onClick={() => OnMovieClick(movie)}
+				variant="link"
+				className="mt-5"
+			>
+				Open
+			</Button>
+		</Card>
 	);
 };
 MovieCard.propTypes = {
 	movie: PropTypes.shape({
-		ImagePath: String,
-		Title: String,
+		ImagePath: PropTypes.string,
+		Title: PropTypes.string,
+		Description: PropTypes.string,
+		Director: PropTypes.shape({
+			Name: PropTypes.string,
+		}),
 	}),
 	OnMovieClick: PropTypes.func,
 };
