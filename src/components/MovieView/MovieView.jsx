30d2a07b1c780e9fb43715dd4c870f2d707050b0
Mezekr W <mezi.edu@tuta.io>
2024-03-17 09:28:52 +0100

    refactor component to view a movie Details

--- src/components/MovieView/MovieView.jsx
+++ src/components/MovieView/MovieView.jsx
@@ -1,28 +1,53 @@
 import PropTypes from 'prop-types';
-import { Button, Card, Row } from 'react-bootstrap';
+import { Button, Card, Col, Row } from 'react-bootstrap';
+import { Link, useParams } from 'react-router-dom';
+import MovieCard from '../MovieCard/MovieCard';
 
-const MovieView = ({ movie, onBackClick }) => {
+const MovieView = ({ movies }) => {
+	const { movieId } = useParams();
+	const movie = movies.find((m) => m._id === movieId);
 	return (
-		<Row className="justify-content-md-center">
-			<Card style={{ width: '20rem' }}>
-				<Card.Img
-					src={movie.ImagePath}
-					alt={movie.Title}
-					className="w-100"
-				/>
-				<Card.Body>
-					<Card.Title>{movie.Title}</Card.Title>
-					<Card.Subtitle>{movie.Description}</Card.Subtitle>
-					<Card.Text>Director: {movie.Director.Name}</Card.Text>
-				</Card.Body>
-				<Button onClick={onBackClick} variant="primary">
-					Back
-				</Button>
-			</Card>
-		</Row>
+		<>
+			<Row className="justify-content-md-center">
+				<Card style={{ width: '20rem' }}>
+					<Card.Img
+						src={movie.ImagePath}
+						alt={movie.Title}
+						className="w-100"
+					/>
+					<Card.Body>
+						<Card.Title>{movie.Title}</Card.Title>
+						<Card.Subtitle>{movie.Description}</Card.Subtitle>
+						<Card.Text>Director: {movie.Director.Name}</Card.Text>
+					</Card.Body>
+					<Card.Footer>
+						<Link to={'/'}>
+							<Button variant="primary">Back</Button>
+						</Link>
+					</Card.Footer>
+				</Card>
+			</Row>
+			<hr />
+			<h2>Similar Movies</h2>
+			<Row>
+				<Col className="mb-5" md={3}>
+					{movies
+						.filter(
+							(m) =>
+								m.Genre.Name === movie.Genre.Name &&
+								m.Title !== movie.Title
+						)
+						.map((m) => {
+							return <MovieCard key={m._id} movie={m} />;
+						})}
+				</Col>
+				;
+			</Row>
+		</>
 	);
 };
 MovieView.propTypes = {
+	movies: PropTypes.arrayOf(Object),
 	movie: PropTypes.shape({
 		Title: String,
 		ImagePath: String,
@@ -30,7 +55,6 @@ MovieView.propTypes = {
 		Director: PropTypes.shape({ Name: String }),
 		ReleaseYear: String,
 	}),
-	onBackClick: PropTypes.func,
 };
 
 export default MovieView;
