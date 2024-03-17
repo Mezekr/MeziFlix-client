e420ce0cd20173ad5d11e24e88616122ce35a950
Mezekr W <mezi.edu@tuta.io>
2024-03-17 09:47:32 +0100

    Refactor the main application component
    - add react routing
    - integrate navigation bar with routing
    - add a feature to add or remove favourite movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
 import MainView from './components/MainView/MainView';
-// import './index.scss';
 
 function App() {
 	return (
 		<>
-			<div className="my-flix">
-				<h1>MeziFlix</h1>
-			</div>
-			<Container>
+			<Container fluid>
 				<MainView />
 			</Container>
 		</>
--- src/components/MainView/MainView.jsx
+++ src/components/MainView/MainView.jsx
@@ -1,20 +1,28 @@
-import { useEffect, useState } from 'react';
-import { Button, Col, Row } from 'react-bootstrap';
+import { createContext, useEffect, useState } from 'react';
+import { Col, Row } from 'react-bootstrap';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import LoginView from '../LoginView/LoginView';
 import MovieCard from '../MovieCard/MovieCard';
 import MovieView from '../MovieView/MovieView';
+import { Navbar } from '../Navbar/Navbar';
+import DeleteUserView from '../Profile/DeleteUserView';
+import ProfileView from '../Profile/ProfileView';
+import UserDataUpdateView from '../Profile/UserDataUpdateView';
 import SignupView from '../SignupView/SignupView';
 
 const MOVIES_API_URL = 'https://meziflix-api-v1.onrender.com/';
+
+export const favMoviesContext = createContext([]);
 
 const MainView = () => {
 	const storedUser = localStorage.getItem('user');
 	const storedToken = localStorage.getItem('token');
 
 	const [movies, setMovies] = useState([]);
-	const [selectedMovie, setSelectedMovie] = useState(null);
 	const [user, setUser] = useState(storedUser ? storedUser : null);
 	const [token, setToken] = useState(storedToken ? storedToken : null);
+	const [favoriteMovies, setFavoriteMovies] = useState([]);
+	const [favMoviesID, setfavMoviesID] = useState([]);
 
 	const searchMovie = async (token) => {
 		const response = await fetch(`${MOVIES_API_URL}movies`, {
@@ -24,114 +32,270 @@ const MainView = () => {
 		setMovies(data);
 	};
 
+	const onLogedOut = () => {
+		setUser(null), setToken(null), localStorage.clear();
+	};
+
 	useEffect(() => {
 		if (!token) return;
 		searchMovie(token);
 	}, [token]);
 
+	useEffect(() => {
+		if (!user) return;
+		fetch(`${MOVIES_API_URL}users/${user.Username}`, {
+			headers: { Authorization: `Bearer ${token}` },
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				if (!data) {
+					setFavoriteMovies();
+				} else {
+					setFavoriteMovies(
+						movies.filter((m) =>
+							data['FavouriteMovies'].includes(m._id)
+						)
+					);
+					setfavMoviesID(data['FavouriteMovies']);
+				}
+			})
+			.catch((err) => console.error(err));
+	});
+
+	const addFavMovie = (movieId) => {
+		if (!user) return;
+		console.log(favMoviesID);
+		if (!favMoviesID.includes(movieId)) {
+			fetch(`${MOVIES_API_URL}users/${user.Username}/movies/${movieId}`, {
+				method: 'PUT',
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			})
+				.then((response) => response.json())
+				.then((data) => {
+					console.log('addFavMovie');
+					console.log(data), alert('Movie added successfully');
+				})
+				.catch((err) => console.error(err));
+		} else {
+			alert('Already added');
+		}
+	};
+	const removeFavMovie = (movieId) => {
+		if (!user) return;
+		if (favMoviesID.includes(movieId)) {
+			fetch(`${MOVIES_API_URL}users/${user.Username}/movies/${movieId}`, {
+				method: 'DELETE',
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			})
+				.then((response) => response.json())
+				.then((data) => {
+					if (!data) {
+						setFavoriteMovies();
+					} else {
+						setFavoriteMovies(
+							movies.filter((m) =>
+								data['FavouriteMovies'].includes(m._id)
+							)
+						);
+						setfavMoviesID(data['FavouriteMovies']);
+					}
+					alert('Movie removed successfully');
+				})
+				.catch((err) => console.error(err));
+		} else {
+			alert('not on Fav yet!');
+		}
+	};
+
 	return (
 		<>
-			<Row className="justify-content-md-center">
-				{!user ? (
-					<>
-						<Col md={5}>
-							<h2>Login</h2>
-							<LoginView
-								onLogin={(user, token) => {
-									setUser(user);
-									setToken(token);
-								}}
-							/>
-
-							<br />
-							<h2 style={{ color: 'white' }}>
-								Sign up as a new user below
-							</h2>
-							<br />
-							<SignupView />
-						</Col>
-					</>
-				) : selectedMovie ? (
-					<>
-						<Row className="justify-content-md-center">
-							<Col md={8}>
-								<MovieView
-									movie={selectedMovie}
-									onBackClick={() => setSelectedMovie(null)}
-								/>
-							</Col>
-						</Row>
-						<hr />
-						<h2>Similar Movies</h2>
-						<Row>
-							<Col className="mb-5" md={3}>
-								{movies
-									.filter(
-										(movie) =>
-											movie.Genre.Name ===
-												selectedMovie.Genre.Name &&
-											movie.Title !== selectedMovie.Title
-									)
-									.map((movie) => {
-										return (
-											<MovieCard
-												key={movie._id}
-												movie={movie}
-												OnMovieClick={(
-													newSelectedMovie
-												) =>
-													setSelectedMovie(
-														newSelectedMovie
-													)
-												}
-											/>
-										);
-									})}
-							</Col>
-							;
-						</Row>
-					</>
-				) : movies?.length <= 0 ? (
-					<div className="empty">
-						<h2> loading...</h2>
-					</div>
-				) : (
-					<>
-						{movies.map((movie) => {
-							return (
-								<Col
-									key={movie._id}
-									className="mb-5"
-									sm={6}
-									md={4}
-									xl={3}
-								>
-									<MovieCard
-										movie={movie}
-										OnMovieClick={(newSelectedMovie) =>
-											setSelectedMovie(newSelectedMovie)
-										}
-									/>
-								</Col>
-							);
-						})}
-					</>
-				)}
+			<Row>
+				<Col>
+					<Navbar user={user} onLogedOut={onLogedOut} />
+				</Col>
 			</Row>
 			<Row className="justify-content-md-center">
-				<Col md={3}>
-					<Button
-						onClick={() => {
-							setUser(null);
-							setToken(null);
-							localStorage.clear();
-						}}
-						variant="secondary"
-						size="lg"
-					>
-						Logout
-					</Button>
-				</Col>
+				<Routes>
+					<Route
+						path="/Signup"
+						element={
+							<>
+								{user ? (
+									<Navigate to={'/'} />
+								) : (
+									<Col md={5}>
+										<SignupView />
+									</Col>
+								)}
+							</>
+						}
+					/>
+					<Route
+						path="/login"
+						element={
+							<>
+								{user ? (
+									<Navigate to={'/'} />
+								) : (
+									<Col md={5}>
+										<LoginView
+											onLogin={(user, token) => {
+												setUser(user);
+												setToken(token);
+											}}
+										/>
+									</Col>
+								)}
+							</>
+						}
+					/>
+					<Route
+						path="/profile"
+						element={
+							<>
+								{!user ? (
+									<Navigate to={'/login'} replace />
+								) : (
+									<Col md={10}>
+										<favMoviesContext.Provider
+											value={favMoviesID}
+										>
+											<ProfileView
+												user={user}
+												movies={movies}
+												favoriteMovies={favoriteMovies}
+												removeFavMovie={removeFavMovie}
+											/>
+										</favMoviesContext.Provider>
+									</Col>
+								)}
+							</>
+						}
+					/>
+					<Route
+						path="/movies/:movieId"
+						element={
+							<>
+								{!user ? (
+									<Navigate to={'/login'} replace />
+								) : movies?.length <= 0 ? (
+									<div className="empty">
+										<h2> Movies List is empty!</h2>
+									</div>
+								) : (
+									<>
+										<Row className="justify-content-md-center">
+											<Col md={8}>
+												<MovieView movies={movies} />
+											</Col>
+										</Row>
+									</>
+								)}
+							</>
+						}
+					/>
+					<Route
+						path="/"
+						element={
+							<>
+								{!user ? (
+									<Navigate to={'/login'} replace />
+								) : movies?.length <= 0 ? (
+									<div className="empty">
+										<h2> Movies List is empty!</h2>
+									</div>
+								) : (
+									<>
+										{movies.map((movie) => {
+											return (
+												<Col
+													key={movie._id}
+													className="mb-5"
+													sm={6}
+													md={4}
+													lg={3}
+													xl={3}
+													xxl={2}
+												>
+													<favMoviesContext.Provider
+														value={favMoviesID}
+													>
+														<MovieCard
+															movie={movie}
+															favMoviesID={
+																favMoviesID
+															}
+															addFavMovie={
+																addFavMovie
+															}
+															removeFavMovie={
+																removeFavMovie
+															}
+														/>
+													</favMoviesContext.Provider>
+												</Col>
+											);
+										})}
+									</>
+								)}
+							</>
+						}
+					/>
+
+					<Route
+						path="/logout"
+						element={
+							<>
+								{!user ? (
+									<Navigate to={'/login'} />
+								) : (
+									<Row className="justify-content-md-center">
+										<Col md={3}>
+											onLogedOut;
+											<LoginView />
+										</Col>
+									</Row>
+								)}
+							</>
+						}
+					/>
+					<Route
+						path="/UserDataUpdateView"
+						element={
+							<>
+								{!user ? (
+									<Navigate to={'/login'} />
+								) : (
+									<Row className="justify-content-md-center">
+										<Col md={3}>
+											<UserDataUpdateView user={user} />
+										</Col>
+									</Row>
+								)}
+							</>
+						}
+					/>
+					<Route
+						path="/DeleteUserView"
+						element={
+							<>
+								{!user ? (
+									<Navigate to={'/login'} />
+								) : (
+									<Row className="justify-content-md-center">
+										<Col md={3}>
+											<DeleteUserView user={user} />
+										</Col>
+									</Row>
+								)}
+							</>
+						}
+					/>
+				</Routes>
 			</Row>
 		</>
 	);
