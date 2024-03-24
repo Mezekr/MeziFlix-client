8b2284c7263a1ef8e103d5557f362f5be0050845
Mezekr W <mezi.edu@tuta.io>
2024-03-24 22:02:32 +0100

    refactor MovieView component which shows details

--- src/components/MovieView/MovieView.jsx
+++ src/components/MovieView/MovieView.jsx
@@ -1,54 +1,41 @@
 import PropTypes from 'prop-types';
-import { Button, Card, Col, Row } from 'react-bootstrap';
+import { Button, Col, Row } from 'react-bootstrap';
 import { Link, useParams } from 'react-router-dom';
 import MovieCard from '../MovieCard/MovieCard';
+import './movieView.scss';
 
 const MovieView = ({ movies }) => {
 	const { movieId } = useParams();
 	const movie = movies.find((m) => m._id === movieId);
 	return (
 		<>
-			<Row xs={1} lg={2} className="g-1">
-				<Card>
-					<Card.Img
-						src={movie.ImagePath}
-						alt={movie.Title}
-						className="w-100 h-100"
-						variant="center"
-					/>
-				</Card>
+			<Row>
+				<div className="movie-wrapper">
+					<div className="movie-thumbnail">
+						<img src={movie.ImagePath} alt={movie.Title} />
+					</div>
 
-				<Card>
-					<Card.Body>
-						<Card.Title>{movie.Title}</Card.Title>
-						<Card.Text>{movie.Description}</Card.Text>
-						<Card.Text>Director: {movie.Director.Name}</Card.Text>
-						<Card.Text>
-							Director Bio: {movie.Director.Bio}
-						</Card.Text>
-						<Card.Text>
-							Director Birth: {movie.Director.Birth}
-						</Card.Text>
-						<Card.Text>Genre: {movie.Genre.Name}</Card.Text>
-						<Card.Text>
-							Genre Description: {movie.Genre.Description}
-						</Card.Text>
-						<Card.Text>Actors: {movie.Actors}</Card.Text>
-					</Card.Body>
-				</Card>
-
-				<Link to={'/'}>
-					<Button
-						variant="primary"
-						className="d-grid gap-2"
-						size="lg"
-					>
-						Back
-					</Button>
-				</Link>
+					<div className="movie-body">
+						<span className="movie-title">{movie.Title}</span>
+						<p>
+							<span>{movie.Genre.Name}</span>
+							<span>{movie.ReleaseYear}</span>
+						</p>
+						<p>
+							<span className="movie-director">
+								{movie.Director.Name}
+							</span>
+						</p>
+						<p className="movie-description">{movie.Description}</p>
+						<span className="movie-actors"> {movie.Actors}</span>
+						<Link to={'/'}>
+							<Button className="trailer">Back</Button>
+						</Link>
+					</div>
+				</div>
 			</Row>
-			<hr />
 			<h2>Similar Movies</h2>
+			<hr />
 			<Row>
 				<Col className="mb-5" md={3}>
 					{movies
